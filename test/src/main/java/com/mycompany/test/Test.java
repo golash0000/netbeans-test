fda4662bea4d30a15f54c537bcd1dbcf99412199
golash0000 <obsequio.kenneth.ampoloquio@gmail.com>
2023-11-06 01:18:49 +0800

    another one

--- test/src/main/java/com/mycompany/test/Test.java
+++ test/src/main/java/com/mycompany/test/Test.java
@@ -13,5 +13,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("test");
+        System.out.println("tanginamo");
     }
 }
